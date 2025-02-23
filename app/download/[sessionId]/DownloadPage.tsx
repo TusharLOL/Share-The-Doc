6a729e01cbee8ef57cc7731fce67bd02a42d48ab
ganeshb2334 <bastapureganesh21@gmail.com>
2025-02-23 18:48:12 +0530

    Updated

--- app/download/[sessionId]/DownloadPage.tsx
+++ app/download/[sessionId]/DownloadPage.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import Toast from "@/components/Toast";
-import { Clipboard } from "lucide-react";
+import { Check, CheckCircle, Clipboard } from "lucide-react";
 import { useQRCode } from "next-qrcode";
+import { EmailIcon, EmailShareButton, LinkedinIcon, LinkedinShareButton, TelegramIcon, TelegramShareButton, WhatsappIcon, WhatsappShareButton } from "next-share";
 import Head from "next/head";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
@@ -15,13 +16,14 @@ interface FileData {
 
 export default function DownloadPage() {
   const { sessionId } = useParams();
+  const navigate = useRouter();
   const { Canvas } = useQRCode();
-  const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
+  const [downloadUrl, setDownloadUrl] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
-
-  useEffect(() => {
-    setDownloadUrl(window.location.href);
-  }, []);
+  const query = new URLSearchParams(window.location.search);
+  const isUser = query.get("user") === "me";
+  const sharableUrl = "https://share-the-doc.vercel.app/download/" + sessionId;
+  const [isSessionCompleted, setIsSessionCompleted] = useState(false);
 
   const handleDownload = async () => {
     if (!sessionId) {
@@ -83,16 +85,16 @@ export default function DownloadPage() {
 
   const handleCopy = () => {
     if (navigator.clipboard && window.isSecureContext) {
-      navigator.clipboard.writeText(downloadUrl || "")
+      navigator.clipboard.writeText(sharableUrl || "")
         .then(() => toast.custom((t: any) => (
           <Toast message="URL copied to clipboard!" t={t} variant="success" />
         )))
         .catch((err) => {
           console.error("Clipboard API failed, using fallback method", err);
-          fallbackCopyTextToClipboard(downloadUrl || "");
+          fallbackCopyTextToClipboard(sharableUrl || "");
         });
     } else {
-      fallbackCopyTextToClipboard(downloadUrl || "");
+      fallbackCopyTextToClipboard(sharableUrl || "");
     }
   };
 
@@ -108,6 +110,37 @@ export default function DownloadPage() {
     ));
   };
 
+  const checkSessionStatus = async () => {
+    try {
+      const response = await fetch("/api/session/" + sessionId, {
+        method: "GET",
+      });
+      if (!response.ok) {
+        setIsSessionCompleted(true);
+      }
+    } catch (error) {
+      console.error("Error checking session status:", error);
+    }
+  }
+
+  useEffect(() => {
+    setDownloadUrl(window.location.href);
+    const interval = setInterval(checkSessionStatus, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    if (isSessionCompleted) {
+      toast.custom((t: any) => (
+        <Toast message="Session expired" t={t} variant="error" />
+      ));
+      timeout = setTimeout(() => {
+        navigate.push("/");
+      }, 1000);
+    }
+    return () => clearTimeout(timeout);
+  }, [isSessionCompleted])
 
   return (
     <>
@@ -117,54 +150,89 @@ export default function DownloadPage() {
       </Head>
       <div className="flex flex-col items-center justify-center my-20 p-4">
         <div className="bg-gray-700 p-8 rounded-lg shadow-lg w-full max-w-md text-center">
-          <h1 className="text-xl font-bold mb-4 border-b">Download Your Files</h1>
-          <div className="flex items-center justify-center mb-4">
+          {
+            isSessionCompleted ? (
+              <div className="flex flex-col justify-center items-center gap-5 my-10">
+                <Check size={32} className="bg-green-500 p-3 h-[150px] w-[150px] rounded-full animate-enter" />
+                <p className="text-xl text-center animate-enter">Download completed. Session Expired!!</p>
+              </div>
+            ) :
+              <>
+                <h1 className="text-xl font-bold mb-4 border-b">Download Your Files</h1>
+                <div className="flex items-center justify-center my-4">
+                  {
+                    downloadUrl ? (
+                      <Canvas
+                        text={sharableUrl || ""}
+                        options={{
+                          type: "svg",
+                          quality: 0.3,
+                          errorCorrectionLevel: "M",
+                          margin: 3,
+                          scale: 4,
+                          width: 200,
+                          color: {
+                            dark: "#010599FF",
+                            light: "#FFBF60FF",
+                          },
+                        }}
+                      />
+                    ) : (
+                      <p>Generating QR Code...</p>
+                    )}
+                </div>
+                <div className="flex items-center justify-center mb-4">
+                  <input
+                    type="text"
+                    readOnly
+                    value={sharableUrl || ""}
+                    className="border p-2 rounded-l-lg w-full text-black"
+                  />
+                  <button
+                    onClick={handleCopy}
+                    className="py-2 px-4 border rounded-r-lg">
+                    <Clipboard
+                      className="cursor-pointer"
+                    />
+                  </button>
+                </div>
+                <div className="space-y-3">
+                  <p className="font-bold">Share the URL with anyone to download the files.</p>
+                  <div className="space-x-3">
+                    <WhatsappShareButton
+                      url={sharableUrl || ""}
+                      title={'Just upload your files and share the link to download them.\nSecure and easy to use.'}
+                    >
+                      <WhatsappIcon size={32} round />
+                    </WhatsappShareButton>
+                    <LinkedinShareButton url={sharableUrl}>
+                      <LinkedinIcon size={32} round />
+                    </LinkedinShareButton>
+                    <TelegramShareButton
+                      url={sharableUrl || ""}
+                      title={'Just upload your files and share the link to download them.\nSecure and easy to use.'}
+                    >
+                      <TelegramIcon size={32} round />
+                    </TelegramShareButton>
+                    <EmailShareButton
+                      url={sharableUrl || ""}
+                      subject={'Share-The-Doc - Download your files'}
+                      body="Just upload your files and share the link to download them.\nSecure and easy to use."
+                    >
+                      <EmailIcon size={32} round />
+                    </EmailShareButton>
+                  </div>
+                </div>
 
-            {downloadUrl ? (
-              <Canvas
-                text={downloadUrl}
-                options={{
-                  type: "svg",
-                  quality: 0.3,
-                  errorCorrectionLevel: "M",
-                  margin: 3,
-                  scale: 4,
-                  width: 200,
-                  color: {
-                    dark: "#010599FF",
-                    light: "#FFBF60FF",
-                  },
-                }}
-              />
-            ) : (
-              <p>Generating QR Code...</p>
-            )}
-
-          </div>
-          <div className="flex items-center justify-center mb-4">
-            <input
-              type="text"
-              readOnly
-              value={downloadUrl || ""}
-              className="border p-2 rounded-l-lg w-full text-black"
-            />
-            <button
-              onClick={handleCopy}
-              className="py-2 px-4 border rounded-r-lg">
-              <Clipboard
-                className="cursor-pointer"
-              />
-            </button>
-          </div>
-
-          <button
-            onClick={handleDownload}
-            disabled={loading}
-            className={`bg-blue-500 py-2 px-4 rounded hover:bg-blue-600 ${loading ? "opacity-50 cursor-not-allowed" : ""
-              }`}
-          >
-            {loading ? "Downloading..." : "Download Files"}
-          </button>
+                {!isUser && <button
+                  onClick={handleDownload}
+                  disabled={loading}
+                  className={`bg-blue-500 py-2 px-4 rounded hover:bg-blue-600 mt-3 ${loading ? "opacity-50 cursor-not-allowed" : ""
+                    }`}
+                >
+                  {loading ? "Downloading..." : "Download Files"}
+                </button>}
+              </>}
         </div>
       </div>
     </>
